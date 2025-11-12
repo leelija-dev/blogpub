28a9b0850cfdb8353ad3e588185554abaae4cc99
susmita <susmitaghosh.leelija@gmail.com>
2025-11-12 14:28:20 +0530

    admin crud done

---
+++ app/Filament/Resources/Admins/Pages/CreateAdmins.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Admins\Pages;
+
+use App\Filament\Resources\Admins\AdminsResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAdmins extends CreateRecord
+{
+    protected static string $resource = AdminsResource::class;
+}
---
+++ app/Filament/Resources/Admins/Tables/AdminsTable.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Filament\Resources\Admins\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+
+class AdminsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')->label('ID')->searchable()->sortable(),
+                TextColumn::make('name')
+                    ->label('Admin Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')->searchable()->sortable(),
+                TextColumn::make('created_at'),
+                ToggleColumn::make('status')
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
