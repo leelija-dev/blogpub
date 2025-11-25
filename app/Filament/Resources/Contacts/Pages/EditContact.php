2a041fb00207d461ca964d08a78f07de2e90374c
Lakshman Pal <lakshman.leelija@gmail.com>
2025-11-25 17:56:22 +0530

    update order mail and add contact data in admin

---
+++ app/Filament/Resources/Contacts/Pages/EditContact.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Contacts\Pages;
+
+use App\Filament\Resources\Contacts\ContactResource;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\ForceDeleteAction;
+use Filament\Actions\RestoreAction;
+use Filament\Resources\Pages\EditRecord;
+
+class EditContact extends EditRecord
+{
+    protected static string $resource = ContactResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            DeleteAction::make(),
+            ForceDeleteAction::make(),
+            RestoreAction::make(),
+        ];
+    }
+    // Remove Save button
+    protected function getFormActions(): array
+    {
+        return []; // hides Save button completely
+    }
+
+    
+}
